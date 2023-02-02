433ce26347965ceec137c13252a666621ca6bb11
webdeveloper1213 <sameera_sulaiman@yahoo.com>
2023-02-02 13:20:51 -0800

    added react icons

--- reduxexample1/src/components/Navbar.js
+++ reduxexample1/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useSelector , useDispatch } from 'react-redux';
-
+import {FaShoppingCart} from 'react-icons/fa';
 
 const Navbar = () => {
 
@@ -11,7 +11,10 @@ const Navbar = () => {
         <span className='reduxStore'>REDUX STORE</span>
         <div className='navigation'>
             <Link to="/">Home</Link>
-            <Link to="/cart" style={{position:'relative'}}>Cart</Link>
+            {/* <Link to="/cart" style={{position:'relative'}}>Cart</Link> */}
+            <Link to='/cart'>
+           <FaShoppingCart />
+            </Link>
             <span className='cartCount'>{item.length}</span>
         </div>
         </div>
